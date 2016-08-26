db7439254cacacfa73e7ae035d1b387ea7187fe3
Kai Xiang <kxiang@pivotal.io>
2016-08-26 16:18:01 -0700

    Add pipeline filter so that it could filter by pipeline names

--- src/concourse-summary/pipeline.cr
+++ src/concourse-summary/pipeline.cr
@@ -8,9 +8,13 @@ class Pipeline
     paused: Bool
   )
 
-  def self.all(client)
+  def self.all(client, filters)
     response = client.get("/api/v1/pipelines")
     raise Unauthorized.new if response.status_code == 401
-    Array(Pipeline).from_json(response.body)
+    all_the_pipes = Array(Pipeline).from_json(response.body)
+    unless filters.empty?
+      all_the_pipes.select! { |p| filters.includes?(p.name) }
+    end
+    all_the_pipes
   end
 end
