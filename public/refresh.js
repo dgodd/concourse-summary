742e12bf0ca81fb871a41cc50b10dfa04c6e78b7
Sam Bryant <srbry@hotmail.com>
2016-11-01 15:12:31 +0000

    add scaling to refresh.js to make it persist through refreshes
    Signed-off-by: Rachael Wonnacott <Rachael.Wonnacott@fil.com>

--- public/refresh.js
+++ public/refresh.js
@@ -1,4 +1,33 @@
 window.refresh_interval = window.refresh_interval || 30;
+var scaletext = function() {
+  $( '.outer' ).each(function ( i, box ) {
+    var canvas = document.createElement('canvas');
+    var ctx = canvas.getContext("2d");
+    var font_family = $('body').css('font-family');
+    var font_size_px = $('body').css('font-size');
+    var font_size = parseInt(font_size_px.split("px")[0], 10);
+    var ctx_font = font_size + "px " + font_family
+    ctx.font = ctx_font
+    var box_text = box.innerText || box.textContent;
+    var split_box_text = box_text.split(/\r?\n/)
+    var last_line = split_box_text[split_box_text.length -1]
+    var width = ctx.measureText(last_line).width;
+    var linewidth = $( box ).width();
+
+    var inner = $( '.inner' );
+    html = '<span class="' + last_line + '" style="font_size: ' + font_size + 'px"></span>',
+    line = $( box ).wrapInner( html ).children()[ 0 ];
+
+    while ( width > linewidth ) {
+      --font_size
+      $( '.' + last_line ).css( 'font-size', font_size );
+      ctx_font = font_size + "px " + font_family
+      ctx.font = ctx_font
+      width = ctx.measureText(last_line).width
+    }
+  });
+};
+scaletext();
 var onerror = function() {
   document.body.innerHTML = '<div class="time">' + Date() + ' (<span id="countdown">' + refresh_interval + '</span>)</div><h1>ERROR</h1>';
 };
@@ -7,6 +36,7 @@ var onsuccess = function(request) {
   doc.documentElement.innerHTML = request.response;
   document.head.innerHTML=doc.head.innerHTML
   document.body.innerHTML=doc.body.innerHTML
+  scaletext()
 };
 setInterval(function() {
   var request = new XMLHttpRequest();
